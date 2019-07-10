4a6d0b8e243c4097eefe5dfbf56b4267c831e8d2
Manish Goregaokar <manishsmail@gmail.com>
2019-07-10 11:27:18 -0700

    bit of docs

--- webxr-api/frame.rs
+++ webxr-api/frame.rs
@@ -14,5 +14,8 @@ use euclid::TypedRigidTransform3D;
 #[cfg_attr(feature = "ipc", derive(serde::Serialize, serde::Deserialize))]
 pub struct Frame {
     /// The transform from the viewer to native coordinates
+    ///
+    /// This is equivalent to the pose of the viewer in native coordinates.
+    /// This is the inverse of the view matrix.
     pub transform: TypedRigidTransform3D<f32, Viewer, Native>,
 }
