232d28e5402e2485b866128d12127fe4143805e5
Daniel Adams <msub2official@gmail.com>
2024-07-17 17:16:14 -1000

    Remove outdated backends and update Cargo.toml files
    Signed-off-by: Daniel Adams <msub2official@gmail.com>

--- webxr/lib.rs
+++ webxr/lib.rs
@@ -10,21 +10,6 @@ pub mod glwindow;
 #[cfg(feature = "headless")]
 pub mod headless;
 
-#[cfg(feature = "googlevr")]
-pub mod googlevr;
-
-#[cfg(feature = "gles")]
-mod gles;
-
-#[cfg(all(feature = "googlevr", target_os = "android"))]
-pub(crate) mod jni_utils;
-
-#[cfg(feature = "magicleap")]
-pub mod magicleap;
-
-#[cfg(feature = "egl")]
-mod egl;
-
 #[cfg(feature = "openxr-api")]
 pub mod openxr;
 
